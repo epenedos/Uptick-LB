c713c7483f3ce7f7d21c101de593c2c0e0475fd7
Eduardo Penedos <epenedos@icloud.com>
2018-03-28 18:29:25 +0100

    Clean UP

--- config/mssqlConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  connConfig: {
-    server: 'DB_SERVER_IPADDRESS',
-    database: 'DATABASE_NAME',
-    user: 'DB_USER',
-    password: 'DB_USER_PASSWORD',
-    port: PORT 
-  },
-};
